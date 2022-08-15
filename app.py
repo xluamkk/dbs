e6e236dba7fa4412ed8153aa57f2c67aef34f821
xluamkk <110704774+xluamkk@users.noreply.github.com>
2022-08-15 14:07:40 +0800

    Add files via upload

---
+++ app.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[29]:
+
+
+from flask import Flask, render_template, request
+
+
+# In[30]:
+
+
+app = Flask(__name__)
+
+
+# In[31]:
+
+
+import joblib
+@app.route("/", methods=["GET","POST"])
+def index():
+    if request.method == "POST":
+        rates = float(request.form.get("rates"))
+        model1 = joblib.load("regression_DBS")
+        r1 = model1.predict([[rates]])
+        model2 = joblib.load("tree_DBS")
+        r2 = model2.predict([[rates]])
+        return(render_template("index.html",result1=r1,result2=r2))
+    else:
+        return(render_template("index.html",result1='waiting',result2='waiting'))
+
+
+# In[32]:
+
+
+if __name__ == '__main__':
+    app.run()
+
+
+# In[ ]:
+
+
+
+
